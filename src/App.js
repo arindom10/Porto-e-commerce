d14095b528300c1e25de94942d5d727c6cbc362a
arindom10 <karindom7@gmail.com>
2023-09-16 00:12:12 +0600

    package install and setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 className="bg-red-400 text-yellow-400">
+        Hello, I am Arindom Kundu!!!
+      </h2>
     </div>
   );
 }
